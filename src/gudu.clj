763818b9f19f3478ffcb5d17b8e73ac7bcb0ff64
Matt Lee <thatismatt@gmail.com>
2013-03-13 21:26:21 +0000

    Rewrite URL generation logic.

--- src/gudu.clj
+++ src/gudu.clj
@@ -6,17 +6,30 @@
   (filter (comp not empty?) ;; ignore leading & multiple slashes
           (str/split url #"/")))
 
-(defn- get-segments [routes [id & ids]]
-  (let [[strings maps] (split-with string? (routes id))
-        route          (first maps)
-        segments-rest  (if (nil? route) [] (get-segments route ids))]
-    (concat strings segments-rest)))
+(declare process-string-segment process-map-segment)
+
+(defn- process-segments
+  ([routes args]
+     (process-segments routes args []))
+  ([[route & _ :as routes] args segments]
+     (if (empty? routes) ;; check anything else?
+       segments
+       (apply (cond
+               (string? route) process-string-segment
+               (map?    route) process-map-segment)
+              [routes args segments]))))
+
+(defn- process-string-segment [[route & routes] args segments]
+  (process-segments routes args (conj segments route)))
+
+(defn- process-map-segment [[route & routes] [arg & args] segments]
+  (process-segments (concat (route arg) routes) args segments))
 
 (defn gu
   "Generate URL"
   [routes]
-  (fn [& ids]
-    (let [segments (get-segments routes ids)]
+  (fn [& args]
+    (let [segments (process-segments [routes] args)]
       (str "/" (str/join "/" segments)))))
 
 (defn du
