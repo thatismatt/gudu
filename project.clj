05a87ea5728348c7dc03d7b76918527bdeef1794
Matt Lee <thatismatt@gmail.com>
2013-03-20 13:39:35 +0000

    Update project URL.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject gudu "0.1.0-SNAPSHOT"
   :description "Generate URL, Degenerate URL"
-  :url "http://example.com/FIXME"
+  :url "http://thatismatt.github.com/gudu"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
