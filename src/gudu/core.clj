912aeaa75b3967cc059cf79687042528ffc255c4
Matt Lee <thatismatt@gmail.com>
2013-03-20 13:09:35 +0000

    Reorder fns to remove need for declare.

--- src/gudu/core.clj
+++ src/gudu/core.clj
@@ -9,8 +9,6 @@
 ;;  "segment" - A definition for the URL pieces that can be matched at a particular point in a route
 ;;      There are different types of segments, e.g. string and map.
 
-(declare match-segments process-segments)
-
 (defprotocol Segment
   (match [segment segments pieces params]
     "## Segment Matching - creating params (from URL pieces)
@@ -23,6 +21,24 @@
 ;; NOTES: Segment instances are responsible for recursing through the
 ;; rest of the pieces/params. match/process should return the params/pieces
 ;; created, and nil if there was no match.
+
+(defn match-segments
+  ([segments pieces]
+     (match-segments segments pieces []))
+  ([[segment & segments :as all-segments] pieces params]
+     (if (empty? all-segments) ;; no more routes to match against
+       (if (empty? pieces)     ;; no more URL pieces to be matched
+         params                ;; so, we've found a match
+         nil)                  ;; unmatched URL pieces, this isn't the route you're looking for
+       (match segment segments pieces params))))
+
+(defn process-segments
+  ([segments params]
+     (process-segments segments params []))
+  ([[segment & segments :as all-segments] params pieces]
+     (if (empty? all-segments) ;; check anything else?
+       pieces
+       (process segment segments params pieces))))
 
 (defn match-static-segment [segment segments [piece & pieces] params]
   (if (= (name segment) piece)
@@ -55,24 +71,6 @@
    :process
    (fn [segment _ [param & params] pieces]
      (process-segments (segment param) params pieces))})
-
-(defn match-segments
-  ([segments pieces]
-     (match-segments segments pieces []))
-  ([[segment & segments :as all-segments] pieces params]
-     (if (empty? all-segments) ;; no more routes to match against
-       (if (empty? pieces)     ;; no more URL pieces to be matched
-         params                ;; so, we've found a match
-         nil)                  ;; unmatched URL pieces, this isn't the route you're looking for
-       (match segment segments pieces params))))
-
-(defn process-segments
-  ([segments params]
-     (process-segments segments params []))
-  ([[segment & segments :as all-segments] params pieces]
-     (if (empty? all-segments) ;; check anything else?
-       pieces
-       (process segment segments params pieces))))
 
 (defn split-url [url]
   (filter (comp not empty?) ;; ignore leading & multiple slashes
