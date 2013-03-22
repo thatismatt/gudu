2e90ea4a52397f248555091403e5684dde7445cf
Matt Lee <thatismatt@gmail.com>
2013-03-22 12:08:24 +0000

    Upgrade to clojure 1.5.0.

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://thatismatt.github.com/gudu"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.0"]]
   :test-selectors {:default (complement :ignore)
                    :all (constantly true)})
