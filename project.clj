c94f34b179c09c0cffc83adcbd13d5b5e92b0089
Matt Lee <thatismatt@gmail.com>
2023-04-23 11:41:10 +0100

    Remove unused test-selectors.

--- project.clj
+++ project.clj
@@ -5,8 +5,6 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :plugins [[lein-codox "0.10.8"]]
-  :test-selectors {:default (complement :ignore)
-                   :all     (constantly true)}
   :codox {:namespaces  [#"^gudu\."]
           :source-uri  "https://github.com/thatismatt/gudu/blob/{version}/{filepath}#L{line}"
           :output-path "doc"})
